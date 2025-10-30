5b62ec667e411ebd70b21a8ed5bb80827ccee281
cadendubois <145172516+cadendubois@users.noreply.github.com>
2025-10-30 15:46:02 -0400

    Initial commit: setup Vite + React project

---
+++ src/pages/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Header(){
+    return(
+        <header className="header">
+            <nav className="nav">
+                <img src="/ylLogo.png"/>
+                <p>Navbar</p>
+            </nav>
+        </header>
+        
+    )
+}
