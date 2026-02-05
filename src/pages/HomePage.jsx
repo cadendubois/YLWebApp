faf3b80224ba0bbaad1407cd012bd8bdceb4c757
cadendubois <cadendubois97@gmail.com>
2026-02-04 20:17:45 -0500

    complete home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,9 +4,13 @@ import Button from '../components/Button'
 function HomePage(){
     return(
         <div className="home-page">
-            <div className="container">
+            <div className="info-button-container">
                 <Button>What is YoungLife?</Button>
+            </div>
+            <div className="logo-container">
                 <Logo size="large" />
+            </div>
+            <div className="auth-button-container">
                 <Button>Sign Up</Button>
                 <Button>Log In</Button>
             </div>
